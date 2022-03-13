ab7c70ed3a8e37e52c5475ab5a4c8df82096b7ed
David <cruz-azul99@hotmail.com>
2022-03-12 23:33:37 -0600

    Cors added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { createConnection } = require('typeorm');
 
 const app = express();
@@ -6,6 +7,7 @@ createConnection();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 const routes = require('./routes/index');
 
